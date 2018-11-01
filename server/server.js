e06b18642c9002ea24c6395529614c9686f9abce
Rashid Siddiqui <rsh_bcs@hotmail.com>
2018-11-01 13:09:00 +0500

    Init Commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const express = require('express');
+
+var app = express();
+const port = process.env.PORT || 3000;
+
+const publicPath = path.join(__dirname, "../public");
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+  console.log(`Started listening to port ${port}`);
+});
+
+module.exports.app = app;
