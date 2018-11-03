0fe031da27a425a796249b2b046be35a0879ed95
Rashid Siddiqui <rsh_bcs@hotmail.com>
2018-11-04 04:55:09 +0500

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -20,12 +20,26 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  //Emit new message event to a client side
+  //Emit new message event to a specific client
   // socket.emit('newMessage', {
   //   from: 'John',
   //   text: 'See you then',
   //   createdAt: 123123
   // });
+
+  //Will show welome message to specific client
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  //Like io.emit, will emit messages to all single users but except the sender(this socket user)
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
 
   //Receive create message event from client side
   socket.on('createMessage', (message) => {
