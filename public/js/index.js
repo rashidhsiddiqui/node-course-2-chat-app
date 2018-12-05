e27407e515ad9317dac4893992e5aeaa3dbdb4c5
Rashid Siddiqui <rsh_bcs@hotmail.com>
2018-12-06 02:32:37 +0500

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,22 @@
 var socket = io();
 
+//auto scrolling when new message added in bottom
+function scrollToBottom () {
+  // Selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child')
+  // Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function () {
   console.log('Connected to server');
 });
@@ -20,6 +37,7 @@ socket.on('newMessage', function (message) {
     createdAt: formattedTime
   });
   $("#messages").append(html);
+  scrollToBottom();
   //------------------------------------
 
   // var li = jQuery('<li></li>');
@@ -40,6 +58,7 @@ socket.on('newLocationMessage', function (message) {
     url: message.url
   });
   $("#messages").append(html);
+  scrollToBottom();
   //------------------------------------
 
   // var li = jQuery('<li></li>');
