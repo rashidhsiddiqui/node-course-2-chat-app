f8df205b00c98ba25a8cbbb26bb712a6e3b9e13e
Rashid Siddiqui <rsh_bcs@hotmail.com>
2019-01-14 00:19:45 +0500

    send messages to only people in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -96,7 +96,7 @@ jQuery('#message-form').on('submit', function (e) {
   var messageTextbox = jQuery('[name=message]');
 
   socket.emit('createMessage', {
-    from: 'User',
+    // from: 'User',
     text: messageTextbox.val()
   }, function () {
     messageTextbox.val('')
